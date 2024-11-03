b85f4fac85136fa7f2fbb1323d0952c2b513ef27
Bhupendra Singh <bhupi29491@gmail.com>
2024-11-03 23:06:52 +0200

    Implemented Jdbc Cursor ItemReader.

--- src/main/java/com/bhupi/spring_batch/chunkprocessing/config/BatchConfig.java
+++ src/main/java/com/bhupi/spring_batch/chunkprocessing/config/BatchConfig.java
@@ -2,6 +2,7 @@ package com.bhupi.spring_batch.chunkprocessing.config;
 
 import com.bhupi.spring_batch.chunkprocessing.domain.Product;
 import com.bhupi.spring_batch.chunkprocessing.domain.ProductFieldSetMapper;
+import com.bhupi.spring_batch.chunkprocessing.domain.ProductRowMapper;
 import com.bhupi.spring_batch.chunkprocessing.reader.ProductNameItemReader;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -11,19 +12,25 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 public class BatchConfig {
+
+    @Autowired
+    public DataSource dataSource;
 
     @Bean
     public ItemReader<String> itemReader() {
@@ -58,6 +65,16 @@ public class BatchConfig {
         return itemReader;
     }
 
+
+    @Bean
+    public ItemReader<Product> jdbcCursorItemReader() {
+        JdbcCursorItemReader<Product> itemReader = new JdbcCursorItemReader<>();
+        itemReader.setDataSource(dataSource);
+        itemReader.setSql("Select * from product_details order by product_id");
+        itemReader.setRowMapper(new ProductRowMapper());
+        return itemReader;
+    }
+
 //    @Bean
 //    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 //        return new StepBuilder("chunkBasedStep1", jobRepository).<String, String>chunk(3, transactionManager)
@@ -73,10 +90,26 @@ public class BatchConfig {
 //                                                                .build();
 //    }
 
+//    @Bean
+//    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("chunkBasedStep1", jobRepository).<Product, Product>chunk(3, transactionManager)
+//                                                                .reader(flatFileItemReader())
+//                                                                .writer(new ItemWriter<Product>() {
+//                                                                    @Override
+//                                                                    public void write(Chunk<? extends Product> chunk) throws Exception {
+//                                                                        System.out.println("Chunk-processing Started");
+//                                                                        chunk.forEach(System.out::println);
+//                                                                        System.out.println("Chunk-processing Ended");
+//                                                                    }
+//                                                                })
+//                                                                .build();
+//    }
+
+
     @Bean
     public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("chunkBasedStep1", jobRepository).<Product, Product>chunk(3, transactionManager)
-                                                                .reader(flatFileItemReader())
+                                                                .reader(jdbcCursorItemReader())
                                                                 .writer(new ItemWriter<Product>() {
                                                                     @Override
                                                                     public void write(Chunk<? extends Product> chunk) throws Exception {
